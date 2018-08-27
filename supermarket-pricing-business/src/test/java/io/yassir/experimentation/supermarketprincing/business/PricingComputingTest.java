ad2d8d4be484dda60a3a983c68773eb0dd254c64
yassir mankari <mankari.yassir@gmail.com>
2018-08-27 15:18:04 +0200

    init TDD

---
+++ supermarket-pricing-business/src/test/java/io/yassir/experimentation/supermarketprincing/business/PricingComputingTest.java
@@ -0,0 +1,33 @@
+package io.yassir.experimentation.supermarketprincing.business;
+
+import org.junit.Test;
+
+/**
+ * @author yassir
+ *
+ * UT to experiment pricing models
+ */
+public class PricingComputingTest {
+
+
+    @Test
+    public void simplePricingTest() {
+        //U.T. to handle simple pricing behavior
+    }
+
+    @Test
+    public void priceByUnitPricingTest() {
+        //U.T. to handle PRICE_BY_UNIT pricing behavior
+    }
+
+    @Test
+    public void buyXGetYFreePricingTest() {
+        //U.T. to handle BUY_X_GET_Y_FREE pricing behavior
+    }
+
+    @Test
+    public void xUnitForYPricingTest() {
+        //U.T. to handle X_UNIT_FOR_Y_PRICE pricing behavior
+    }
+
+}
