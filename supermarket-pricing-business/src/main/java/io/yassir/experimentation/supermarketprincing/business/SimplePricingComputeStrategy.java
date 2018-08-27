e89833db3ff448b9ce1817b157215641671cee32
yassir mankari <mankari.yassir@gmail.com>
2018-08-27 17:57:14 +0200

    resolve TDD test case

--- supermarket-pricing-business/src/main/java/io/yassir/experimentation/supermarketprincing/business/SimplePricingComputeStrategy.java
+++ supermarket-pricing-business/src/main/java/io/yassir/experimentation/supermarketprincing/business/SimplePricingComputeStrategy.java
@@ -3,17 +3,21 @@ package io.yassir.experimentation.supermarketprincing.business;
 import io.yassir.experimentation.supermarketprincing.model.PricingRequest;
 import io.yassir.experimentation.supermarketprincing.model.PricingResponse;
 
+import java.math.BigDecimal;
 import java.util.function.Function;
 
 public class SimplePricingComputeStrategy implements Function<PricingRequest, PricingResponse> {
 
     /**
      * Apply simple pricing behavior
+     *
      * @param pricingRequest
      * @return PricingResponse
      */
     @Override
     public PricingResponse apply(PricingRequest pricingRequest) {
-        return null;
+        BigDecimal price =  pricingRequest.getProduct().getPrice();
+        double unit = pricingRequest.getUnit();
+        return new PricingResponse(pricingRequest, price.multiply(BigDecimal.valueOf(unit)));
     }
 }
