9d53d85236780271720239b1e083a2d1f14d1fba
yassir mankari <mankari.yassir@gmail.com>
2018-08-27 14:59:56 +0200

    add hashcode equals toString() to Product.java

--- supermarket-pricing-model/src/main/java/io/yassir/experimentation/supermarketprincing/model/Product.java
+++ supermarket-pricing-model/src/main/java/io/yassir/experimentation/supermarketprincing/model/Product.java
@@ -4,6 +4,7 @@ import io.yassir.experimentation.supermarketprincing.model.Enum.DiscountType;
 import lombok.Getter;
 
 import java.math.BigDecimal;
+import java.util.Objects;
 
 @Getter
 public class Product {
@@ -12,11 +13,33 @@ public class Product {
     private final BigDecimal price;
     private final DiscountType discountType;
 
-    public Product(String name, BigDecimal price, DiscountType pricingType) {
-        this.ref = name;
+    public Product(String ref, BigDecimal price, DiscountType pricingType) {
+        this.ref = ref;
         this.price = price;
         this.discountType = pricingType;
     }
 
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (!(o instanceof Product)) return false;
+        Product product = (Product) o;
+        return Objects.equals(ref, product.ref) &&
+                Objects.equals(price, product.price) &&
+                discountType == product.discountType;
+    }
 
+    @Override
+    public int hashCode() {
+        return Objects.hash(ref, price, discountType);
+    }
+
+    @Override
+    public String toString() {
+        return "Product{" +
+                "ref='" + ref + '\'' +
+                ", price=" + price +
+                ", discountType=" + discountType +
+                '}';
+    }
 }
