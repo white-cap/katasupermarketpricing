5dcf073dfa7c73e3c024e3b2fe7f8c8dd14058dd
yassir mankari <mankari.yassir@gmail.com>
2018-08-27 19:17:51 +0200

    refactoring storing on package strategy all pattern elements

---
+++ supermarket-pricing-business/src/main/java/io/yassir/experimentation/supermarketprincing/business/strategy/Context.java
@@ -0,0 +1,34 @@
+package io.yassir.experimentation.supermarketprincing.business.strategy;
+
+import io.yassir.experimentation.supermarketprincing.business.io.yassir.experimentation.supermarketprincing.business.exception.PricingComputeException;
+import io.yassir.experimentation.supermarketprincing.model.PricingRequest;
+import io.yassir.experimentation.supermarketprincing.model.PricingResponse;
+
+/**
+ * @author yassir
+ * <p>
+ * Context strategy
+ */
+public class Context {
+
+    private CustomFunctionPricing strategy;
+
+    /**
+     * Constructor context
+     *
+     * @param strategy the strategy
+     */
+    public Context(CustomFunctionPricing strategy) {
+        this.strategy = strategy;
+    }
+
+    /**
+     * Compute pricing
+     *
+     * @param pricingRequest
+     * @return PricingResponse
+     */
+    public PricingResponse compute(PricingRequest pricingRequest) throws PricingComputeException {
+        return strategy.apply(pricingRequest);
+    }
+}
---
+++ supermarket-pricing-business/src/main/java/io/yassir/experimentation/supermarketprincing/business/strategy/SimplePricingComputeStrategy.java
@@ -0,0 +1,29 @@
+package io.yassir.experimentation.supermarketprincing.business.strategy;
+
+import io.yassir.experimentation.supermarketprincing.business.io.yassir.experimentation.supermarketprincing.business.exception.PricingComputeException;
+import io.yassir.experimentation.supermarketprincing.model.PricingRequest;
+import io.yassir.experimentation.supermarketprincing.model.PricingResponse;
+
+import java.math.BigDecimal;
+
+/**
+ * @author yassir
+ * simple pricing impl strategy
+ */
+public class SimplePricingComputeStrategy implements CustomFunctionPricing {
+
+
+    /**
+     * Apply simple pricing behavior
+     * @param pricingRequest request object (DTO)
+     * @return PricingResponse
+     * @throws PricingComputeException Exception on handle pricing compute
+     */
+    @Override
+    public PricingResponse apply(PricingRequest pricingRequest) throws PricingComputeException {
+        this.validateUnit(pricingRequest);
+        BigDecimal price =  pricingRequest.getProduct().getPrice();
+        double unit = pricingRequest.getUnit();
+        return new PricingResponse(pricingRequest, price.multiply(BigDecimal.valueOf(unit)));
+    }
+}
