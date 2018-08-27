bd475e0486aba936c3dfeb94db507482bcfd3967
yassir mankari <mankari.yassir@gmail.com>
2018-08-27 22:37:01 +0200

    Add generic impl method calculate Amount price * unit

--- supermarket-pricing-business/src/main/java/io/yassir/experimentation/supermarketprincing/business/strategy/CustomFunctionPricing.java
+++ supermarket-pricing-business/src/main/java/io/yassir/experimentation/supermarketprincing/business/strategy/CustomFunctionPricing.java
@@ -4,6 +4,8 @@ import io.yassir.experimentation.supermarketprincing.business.io.yassir.experime
 import io.yassir.experimentation.supermarketprincing.model.PricingRequest;
 import io.yassir.experimentation.supermarketprincing.model.PricingResponse;
 
+import java.math.BigDecimal;
+
 /**
  * @author yassir
  * The same as Function 1.8 JDK but allow us to throws exceptions
@@ -20,9 +22,28 @@ public interface CustomFunctionPricing {
      */
     PricingResponse apply(PricingRequest pricingRequest) throws PricingComputeException;
 
+    /**
+     * validator of unit must be superior of zero
+     *
+     * @param pricingRequest pricing Request
+     * @throws PricingComputeException
+     */
     default void validateUnit(PricingRequest pricingRequest) throws PricingComputeException {
         if (pricingRequest.getUnit() == 0)
             throw new PricingComputeException("can't handle 0 unit");
+    }
+
+    /**
+     * classic calculator of price
+     *
+     * @param price price
+     * @param unit  unit
+     * @return
+     */
+    default BigDecimal calculateAmount(BigDecimal price, double unit) {
+        BigDecimal amount;
+        amount = price.multiply(BigDecimal.valueOf(unit));
+        return amount;
     }
 
 }
